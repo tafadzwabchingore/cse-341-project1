d9f2136888116edc9369bd93b9dc875ceadb921c
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-05-10 15:54:55 -0600

    Initial commit part 2

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mongodb = require('./data/database');
+
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+else {
+    app.listen(port, () => {console.log(`Running on port ${port}`)});
+}
+});
