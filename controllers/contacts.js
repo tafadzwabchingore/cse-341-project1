e0be085b2f6ffd237f79141ee7dd82c431038265
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-05-17 16:49:05 -0600

    week2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,7 +6,7 @@ const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(204).json(contacts);
+        res.status(200).json(contacts);
     });
 };
 
