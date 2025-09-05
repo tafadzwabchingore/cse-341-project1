6c4327441f69c14823cc66c9058a20ff54c3b0bf
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-09-05 12:45:30 -0600

    W01 Project: Contacts Part 1

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongodb = require('./data/database');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => {console.log(`Server is running on localhost:${PORT}`);});
+//app.listen(port, () => {console.log(`Server is running on localhost:${port}`);});
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+else {
+    app.listen(port, () => {console.log(`Database is listening and running on port ${port}`)});
+}
+});
