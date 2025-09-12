1cbe6ffe64c78f0bcb8d02ea445091ac9641e00d
Tafadzwa Chingore <tafadzwabchingore@gmail.com>
2025-09-12 10:51:50 -0600

    added post, put, delete, installed and added swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,10 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createContact); //used to create something
+
+router.put('/:id', contactsController.updateContact); //Put/Patch used to update. Can be used interchangebly
+
+router.delete('/:id', contactsController.deleteContact); //removing something
+
 module.exports = router;
